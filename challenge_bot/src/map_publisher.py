75743217c36fcc30d7c5d4612a05201d74005d03
greenteawarrior <emily.wang@students.olin.edu>
2014-11-17 20:18:38 -0500

    ramp, position, and sample callbacks work! (testing with rosrun and a 2x4 map)

--- challenge_bot/src/map_publisher.py
+++ challenge_bot/src/map_publisher.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
+# Emily Wang, Eric Schneider
+# Computational Robotics, Fall 2014, Olin College, taught by Paul Ruvolo
+# This code is responsible for the mapping for our challenge bot. The map 
+# keeps track of where the robot has been, the ramp location, and sample locations
+# using a ROS OccupancyGrid.
+
 import rospy
 from nav_msgs.msg import MapMetaData, OccupancyGrid
-
-# Callbacks should...:
-# Store the ramp location
-# Store where we've been
-# Store where samples are
 
 class MapPublisher():
     def __init__(self):
         rospy.init_node("map_publisher")
 
+        # ROS page on OccupancyGrid
+        # http://docs.ros.org/api/nav_msgs/html/msg/OccupancyGrid.html
+
+        # Paul's create_map.py in the occupanygrid_mapping package in comprobo2014 
+        # has also been a helpful example/reference for the map things.
         self.map = OccupancyGrid()
 
         # header stuff
@@ -22,36 +28,61 @@ class MapPublisher():
         # info stuff
         self.map.info.origin.position.x = 10
         self.map.info.origin.position.y = 10
-        self.map.info.width = 3
-        self.map.info.height = 3
+        self.map.info.width = 4
+        self.map.info.height = 2
         self.map.info.resolution = .01 #m/cell
         self.map.data = [0] * self.map.info.height * self.map.info.width # that row-major order
 
+        # occupancy vals for ramps, where we've been, and where the samples are
+        self.RAMP_OCCUPANCY_VAL = 30
+        self.HAVEBEEN_OCCUPANCY_VAL = 10
+        self.SAMPLE_OCCUPANCY_VAL = 20
+
         # publisher/subscriber stuff
         self.map_pub = rospy.Publisher("map",OccupancyGrid)
+        # TODO: edit and uncomment these lines when these subscribers have been made
         # rospy.Subscriber("current_pos", subscribertype, somecallback, queue_size=1)
         # rospy.Subscriber("sample_finder", subscribertype, somecallback, queue_size=1)
 
-        # mark_ramp()
+        # TODO: use the correct value for the ramp position
+        self.mark_ramp(1, 0)
+
+        # testing testing; 
+        # TODO: get rid of the these lines after
+        # demo-ing this map publisher to Franzee
+        self.pos_cb(1, 1)
+        self.sample_cb(1, 3)
 
     def publish_map(self):
         # handy helper function to call whenever you want to update the map
         self.map.header.seq += 1
         self.map_pub.publish(self.map)
 
-    def pos_cb(self):
-        # fill in squares where you are
+    def row_major_idx(self, x, y):
+        # handy helper function for determining the index of the map list given
+        # x and y values. utilizes row major order
+        return self.map.info.width*x + y
+ 
+    def pos_cb(self, pos_x, pos_y):
+        # fill in squares where you are with HAVEBEEN_OCCUPANCY_VAL
+        idx = self.row_major_idx(pos_x, pos_y)
+        self.map.data[idx] = self.HAVEBEEN_OCCUPANCY_VAL 
         self.publish_map()
 
-    def sample_cb(self):
-        # fill in squares where sample is 
+    def sample_cb(self, sample_x, sample_y):
+        # fill in squares where sample is with SAMPLE_OCCUPANCY_VAL
+        idx = self.row_major_idx(sample_x, sample_y)
+        self.map.data[idx] = self.SAMPLE_OCCUPANCY_VAL 
         self.publish_map()
 
-    def mark_ramp(self):
-        # fills in where the ramp is
+    def mark_ramp(self, ramp_x, ramp_y):
+        # encode where the ramp is with RAMP_OCCUPANCY_VAL
+        idx = self.row_major_idx(ramp_x, ramp_y)
+        self.map.data[idx] = self.RAMP_OCCUPANCY_VAL 
         self.publish_map()
 
     def run(self):
+        # rosrun like you mean it
         r = rospy.Rate(2)
         while not(rospy.is_shutdown()):
             self.publish_map()
