a22034405ce14c3c1aa855eef8286b848a3069ef
greenteawarrior <emily.wang@students.olin.edu>
2014-11-22 22:38:57 -0500

    map updates now take the resolution into account ^_^

--- challenge_bot/src/map_publisher.py
+++ challenge_bot/src/map_publisher.py
@@ -8,7 +8,7 @@
 
 import rospy
 from nav_msgs.msg import MapMetaData, OccupancyGrid
-from challenge_msgs.srv import PointRequest
+from challenge_msgs.srv import PointRequest, PointRequestResponse
 
 class MapPublisher():
     def __init__(self):
@@ -34,11 +34,13 @@ class MapPublisher():
 
         # info stuff
         # TODO: what should we do about the origin...
-        self.map.info.origin.position.x = 10
-        self.map.info.origin.position.y = 10
-        self.map.info.width = 4
-        self.map.info.height = 2
-        self.map.info.resolution = .01 #m/cell
+        self.map.info.origin.position.x = 0
+        self.map.info.origin.position.y = 0
+
+        # TODO: fix the resolution issues... cells and pixels and things
+        self.map.info.width = 40
+        self.map.info.height = 20
+        self.map.info.resolution = .1 #m/cell
         self.map.data = [0] * self.map.info.height * self.map.info.width # that row-major order
 
         # occupancy vals for ramps, where we've been, and where the samples are
@@ -54,8 +56,7 @@ class MapPublisher():
 
         # TODO: use the correct value for the ramp position
         self.mark_ramp(1, 0)
-
-        rospy.spin()
+        print 'marked that ramp'
 
     def publish_map(self):
         # handy helper function to call whenever you want to update the map
@@ -64,10 +65,16 @@ class MapPublisher():
 
     def row_major_idx(self, x, y):
         # handy helper function for determining the index of the map list given
+
+        # resolution things,
+        x = x/self.map.info.resolution
+        y = y/self.map.info.resolution      
+
         # x and y values. utilizes row major order
-        return self.map.info.width*x + y
+        return self.map.info.width*int(x) + int(y)
 
     def handle_pos_service(self, req):
+        print req
         return self.pos_cb(req.point.x, req.point.y)
  
     def pos_cb(self, pos_x, pos_y):
@@ -75,8 +82,10 @@ class MapPublisher():
         idx = self.row_major_idx(pos_x, pos_y)
         self.map.data[idx] = self.HAVEBEEN_OCCUPANCY_VAL 
         self.publish_map()
+        return PointRequestResponse()
 
     def handle_sample_pos_service(self, req):
+        print req
         return self.sample_cb(req.point.x, req.point.y)
 
     def sample_cb(self, sample_x, sample_y):
@@ -84,6 +93,7 @@ class MapPublisher():
         idx = self.row_major_idx(sample_x, sample_y)
         self.map.data[idx] = self.SAMPLE_OCCUPANCY_VAL 
         self.publish_map()
+        return PointRequestResponse()
 
     def mark_ramp(self, ramp_x, ramp_y):
         # encode where the ramp is with RAMP_OCCUPANCY_VAL
