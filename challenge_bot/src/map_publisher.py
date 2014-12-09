39b54d63846a13a6cd33da4cb94655e392b71378
eschneider1992 <franz.eric.schneider@gmail.com>
2014-12-08 19:10:15 -0500

    Made a service that will print the current non-zero map positions

--- challenge_bot/src/map_publisher.py
+++ challenge_bot/src/map_publisher.py
@@ -8,6 +8,7 @@
 
 import rospy
 from nav_msgs.msg import MapMetaData, OccupancyGrid
+from std_srvs.srv import Empty
 from challenge_msgs.srv import PointRequest, PointRequestResponse
 from challenge_msgs.srv import SamplePoint, SamplePointResponse
 
@@ -28,6 +29,9 @@ class MapPublisher():
         # set up the service for updating the map with sample locations
         sample_s = rospy.Service('add_sample_pos_to_map', SamplePoint, self.handle_sample_pos_service)
 
+        # set up the service for updating the map with sample locations
+        print_s = rospy.Service('print_all_map_values', Empty, self.print_all_values)
+
         # header stuff
         self.map.header.seq = 0 # increment this every time we publish the map
         self.map.header.stamp = rospy.Time.now()
@@ -45,7 +49,6 @@ class MapPublisher():
         # occupancy vals for ramps, where we've been, and where the samples are
         self.RAMP_OCCUPANCY_VAL = 30
         self.HAVEBEEN_OCCUPANCY_VAL = 10
-        # self.SAMPLE_OCCUPANCY_VAL = 20
         self.SAMPLE_OCCUPANCY_VALS = {'a':20, 'b':21, 'c':22, 'd':23, 'g':24, 'f':25}
 
         # publisher/subscriber stuff
@@ -84,25 +87,48 @@ class MapPublisher():
         return self.pos_cb(req.point.x, req.point.y)
  
     def pos_cb(self, pos_x, pos_y):
-        # fill in squares where you are with HAVEBEEN_OCCUPANCY_VAL
+        """
+        fill in squares where you are with HAVEBEEN_OCCUPANCY_VAL
+        """
         self.set_value(pos_x, pos_y, self.HAVEBEEN_OCCUPANCY_VAL)
         self.publish_map()
         return PointRequestResponse()
 
     def handle_sample_pos_service(self, req):
-        return self.sample_cb(req.point.x, req.point.y, req.fiducial)
+        if req.fiducial in self.SAMPLE_OCCUPANCY_VALS.keys():
+            return self.sample_cb(req.point.x, req.point.y, req.fiducial)
+        else:
+            return rospy.logwarn('You failed to pass in a valid string')
 
     def sample_cb(self, sample_x, sample_y, sample_f_str):
-        # fill in squares where sample is with the appropriate SAMPLE_OCCUPANCY_VAL for sample_f_str
+        """
+        fill in squares where sample is with the appropriate SAMPLE_OCCUPANCY_VAL for sample_f_str
+        """
         self.set_value(sample_x, sample_y, self.SAMPLE_OCCUPANCY_VALS[sample_f_str]) 
         rospy.loginfo("Sample %s ahoy at (%f, %f)!", sample_f_str, sample_x, sample_y)
         self.publish_map()
         return SamplePointResponse()
 
     def mark_ramp(self, ramp_x, ramp_y):
-        # encode where the ramp is with RAMP_OCCUPANCY_VAL
+        """
+        encode where the ramp is with RAMP_OCCUPANCY_VAL
+        """
         self.set_value(ramp_x, ramp_y, self.RAMP_OCCUPANCY_VAL)
         self.publish_map()
+
+    def print_all_values(self, req):
+        print "GOT HERE"
+        print_string = "Current map values, in meters\n"
+        for x in range(self.map.info.width):
+            for y in range(self.map.info.height):
+                idx = self.row_major_idx(x, y)
+                if self.map.data[idx] > 0:
+                    x_meters = x * self.map.info.resolution + self.map.info.origin.position.x
+                    y_meters = y * self.map.info.resolution + self.map.info.origin.position.y
+                    print_string += "(%f, %f) has value %d\n" % (x_meters, y_meters,
+                                                                 self.map.data[idx])
+        rospy.loginfo(print_string)
+        return []
 
     def run(self):
         # rosrun like you mean it
